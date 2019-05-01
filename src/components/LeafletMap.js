5c80f6740524020ed025a233e506001fe771cf26
megos <gmegos.27@gmail.com>
2019-05-01 15:21:13 +0900

    Fetch csv data

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import csv from 'csvtojson'
 import './LeafletMap.css'
 
 export default class LeafletMap extends Component {
   state = {
     lat: 31.5967656,
     lng: 130.5552324,
-    zoom: 11,
+    zoom: 9,
+    data: [],
+  }
+
+  componentDidMount() {
+    fetch('../data/3-16_takutikouhai.csv')
+      .then((res) => res.text())
+      .then((csvData) => {
+        csv()
+          .fromString(csvData)
+          .then((data) => {
+            this.setState({ data})
+          })
+      })
   }
 
   render() {
@@ -17,11 +31,16 @@ export default class LeafletMap extends Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {this.state.data.map((d, idx) => (
+          <Marker key={idx} autoPan position={[
+            d.POINT_Y,
+            d.POINT_X,
+          ]}>
+            <Popup>
+              {d["名称"]}
+            </Popup>
+          </Marker>
+        ))}
       </Map>
     )
   }
