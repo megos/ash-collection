6de9971522422c43cc5c4c57f958b0771b9d242d
megos <gmegos.27@gmail.com>
2019-05-01 16:12:17 +0900

    Add marker cluster

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
 import csv from 'csvtojson'
 import './LeafletMap.css'
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 export default class LeafletMap extends Component {
   state = {
     lat: 31.5967656,
     lng: 130.5552324,
-    zoom: 9,
+    zoom: 11,
+    maxZoom: 18,
     data: [],
   }
 
@@ -25,22 +36,34 @@ export default class LeafletMap extends Component {
 
   render() {
     const position = [this.state.lat, this.state.lng]
+    const bounds = L.latLngBounds([
+      [31.5968539, 130.5571392],
+      [31.284584, 130.744951],
+    ])
+    this.state.data.forEach((d) => {
+      bounds.extend([
+        d.POINT_Y,
+        d.POINT_X,
+      ])
+    })
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map center={position} zoom={this.state.zoom} maxZoom={this.state.maxZoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {this.state.data.map((d, idx) => (
-          <Marker key={idx} autoPan position={[
-            d.POINT_Y,
-            d.POINT_X,
-          ]}>
-            <Popup>
-              {d["名称"]}
-            </Popup>
-          </Marker>
-        ))}
+        <MarkerClusterGroup>
+          {this.state.data.map((d, idx) => (
+            <Marker key={idx} position={[
+              d.POINT_Y,
+              d.POINT_X,
+            ]}>
+              <Popup>
+                {d["名称"]}
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
       </Map>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import 'leaflet/dist/leaflet.css'
+import 'react-leaflet-markercluster/dist/styles.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
