9e4f4c35f9b65a9a65ad6ce3c41e95471d809498
megos <gmegos.27@gmail.com>
2019-05-03 10:19:02 +0900

    Add getCurrentPosition

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import { Fab, Icon } from 'react-onsenui'
 
 export default class Location extends Component {
+  constructor(props) {
+    super(props)
+    this.getCurrentPosition = this.getCurrentPosition.bind(this)
+  }
+
+  getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos)
+    })
+  }
+
   render() {
     return (
       <Fab position="bottom right">
-        <Icon icon="md-gps-dot" size={26} />
+        <Icon icon="md-gps-dot" size={26} onClick={() => this.getCurrentPosition()} />
       </Fab>
     )
   }
