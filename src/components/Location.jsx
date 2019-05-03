4967177c3b8af72d555e363b3f69ff2cbc84ce7f
megos <gmegos.27@gmail.com>
2019-05-03 15:30:18 +0900

    Add load animation

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -6,24 +6,39 @@ import PropTypes from 'prop-types'
 export default class Location extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isLoading: false,
+    }
     this.getCurrentPosition = this.getCurrentPosition.bind(this)
   }
 
   getCurrentPosition() {
+    this.setState({ isLoading: true })
     navigator.geolocation.getCurrentPosition((pos) => {
+      this.setState({ isLoading: false })
       const { onPositionChange } = this.props
       if (onPositionChange) {
         onPositionChange(pos)
       }
     }, () => {
+      this.setState({ isLoading: false })
       ons.notification.toast('現在地を取得できませんでした', { timeout: 2000 })
     })
   }
 
   render() {
+    const { isLoading } = this.state
     return (
-      <Fab position="bottom right">
-        <Icon icon="md-gps-dot" size={26} onClick={() => this.getCurrentPosition()} />
+      <Fab
+        position="bottom right"
+        disabled={isLoading}
+      >
+        <Icon
+          icon={isLoading ? 'md-spinner' : 'md-gps-dot'}
+          spin={isLoading}
+          size={26}
+          onClick={() => this.getCurrentPosition()}
+        />
       </Fab>
     )
   }
