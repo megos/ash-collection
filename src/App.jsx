57d9fab3e3667452b04b2fa0af1e2b5fd3456c29
megos <gmegos.27@gmail.com>
2019-05-06 00:54:13 +0900

    Fix to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import TopPage from './pages/TopPage'
 import AboutPage from './pages/AboutPage'
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route path="/" exact component={TopPage} />
-        <Route path="/about/" component={AboutPage} />
-      </Router>
-    )
-  }
-}
+export default () => (
+  <Router>
+    <Route path="/" exact component={TopPage} />
+    <Route path="/about/" component={AboutPage} />
+  </Router>
+)
