1ec0113b16c2a3bdf7faa2d3ac76c2f0c9c2b046
megos <gmegos.27@gmail.com>
2019-05-05 23:58:52 +0900

    Add toolbar to about page

---
+++ src/components/AboutToolbar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Toolbar, BackButton } from 'react-onsenui'
+import { withRouter } from 'react-router'
+
+class AboutToolbar extends Component {
+  state = {
+    title: 'このアプリについて',
+  }
+
+  handleClick = () => {
+    // eslint-disable-next-line react/prop-types
+    const { history } = this.props
+    // TODO: if no history
+    history.goBack()
+  }
+
+  render() {
+    const { title } = this.state
+    return (
+      <Toolbar>
+        <div className="left">
+          <BackButton onClick={this.handleClick}>
+            戻る
+          </BackButton>
+        </div>
+        <div className="center">
+          {title}
+        </div>
+      </Toolbar>
+    )
+  }
+}
+
+export default withRouter(AboutToolbar)
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { Page } from 'react-onsenui'
+import AboutToolbar from '../components/AboutToolbar'
 
 export default class AboutPage extends Component {
   render() {
     return (
-      <Page>
+      <Page
+        renderToolbar={() => <AboutToolbar />}
+      >
         This is info page.
       </Page>
     )
