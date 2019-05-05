d28361f348de70360235ec688acc7dc4317d68d0
megos <gmegos.27@gmail.com>
2019-05-05 16:28:06 +0900

    Add back button show condition

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -17,6 +17,11 @@ export default class Toolbar extends Component {
     const { title } = this.state
     return (
       <OnsToolbar>
+        {this.props.navigator.pages.length > 1 &&
+        <div className="left">
+          <BackButton onClick={this.popPage.bind(this)}>戻る</BackButton>
+        </div>
+        }
         <div className="center">
           {title}
         </div>
