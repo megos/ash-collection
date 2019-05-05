99d6d961381fd9684878fa4ae182fc0187b0c755
megos <gmegos.27@gmail.com>
2019-05-06 00:50:05 +0900

    Add content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Page } from 'react-onsenui'
 import AboutToolbar from '../components/AboutToolbar'
+import './AboutPage.css'
 
 export default class AboutPage extends Component {
   state = {
@@ -44,26 +45,39 @@ export default class AboutPage extends Component {
       <Page
         renderToolbar={() => <AboutToolbar />}
       >
-        <p>鹿児島市の灰を捨てる場所（宅地内降灰指定置場）を探すためのアプリです。</p>
-        <h3>リンク集</h3>
-        <ul>
-          {links.map((link, key) => (
-            <li key={key}>
-              <a href={link.url} target="_blank" rel="noreferrer noopener">{link.title}</a>
-            </li>
-          ))}
-        </ul>
-        <h3>使ったもの</h3>
-        <ul>
-          {refs.map((ref, key) => (
-            <li key={key}>
-              <a href={ref.url} target="_blank" rel="noreferrer noopener">{ref.title}</a>
-              <ul>
-                <li>{ref.content}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
+        <div className="about-page">
+          <p>鹿児島市の降灰を捨てる場所（宅地内降灰指定置場）を探すためのアプリです。</p>
+          <ul>
+            <li>灰を捨てる場所がわからない</li>
+            <li>近くの宅地内降灰指定置場を確認したい</li>
+          </ul>
+          <p>といったときにご利用ください。</p>
+          <h3>使い方</h3>
+          <p>地図を自由に操作して探すだけでなく、右下のGPSアイコンをタップして現在地に飛ぶこともできます。 </p>
+          <p>※GPSを利用する場合は、このアプリでGPSを使えるように許可する必要があります。</p>
+          <p>看板をタップすると、その置場の名前を確認することができます。</p>
+          <h3>注意</h3>
+          <p>このアプリは非公式です。鹿児島市とは関係ありませんのでご注意ください。</p>
+          <h3>リンク集</h3>
+          <ul>
+            {links.map((link, key) => (
+              <li key={key}>
+                <a href={link.url} target="_blank" rel="noreferrer noopener">{link.title}</a>
+              </li>
+            ))}
+          </ul>
+          <h3>使ったもの</h3>
+          <ul>
+            {refs.map((ref, key) => (
+              <li key={key}>
+                <a href={ref.url} target="_blank" rel="noreferrer noopener">{ref.title}</a>
+                <ul>
+                  <li>{ref.content}</li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </Page>
     )
   }
